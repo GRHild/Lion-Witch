b8443706fe5fcde8a0a8089983888d52546e7c26
Jesse Kevon <jessekevon@Jesses-MacBook-Pro.local>
2013-09-30 11:28:08 -0500

    add passing test

--- app/controllers/wardrobe_items_controller.rb
+++ app/controllers/wardrobe_items_controller.rb
@@ -1,5 +1,10 @@
 class WardrobeItemsController < ApplicationController
+
   def index
     @wardrobe_items = WardrobeItem.all
+  end
+
+  def show
+    @wardrobe_item = WardrobeItem.find(params[:id])
   end
 end
